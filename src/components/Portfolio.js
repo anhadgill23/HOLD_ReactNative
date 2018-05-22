e399199dc091b1b8c92617a55e18577732a81a9f
Alex <ajollid@gmail.com>
2018-05-22 13:37:30 -0700

    Add moment, add fetch data method to price chart

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,21 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, TextInput, KeyboardAvoidingView, Button } from 'react-native';
+import { Text, View } from 'react-native';
+import PriceChart from './PriceChart';
 
-const styles = StyleSheet.create( {
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-} );
 
 export default class Login extends React.Component {
   render() {
     return (
-      <KeyboardAvoidingView style={styles.container} behavior="padding">
+      <View>
         <Text>Hi! {this.props.navigation.getParam( 'username', '' )}</Text>
-      </KeyboardAvoidingView>
+        <PriceChart />
+      </View>
     );
   }
 }
